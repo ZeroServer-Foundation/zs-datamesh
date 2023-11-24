2890e87787a517c86f29acf4e9df9dbb19e51aeb
wally@ <wally@>
2023-11-24 13:44:53 -0800

    sync z@d@h1: base classes integrated with downstream web repo

--- src/python/zeroserver_datamesh/__init__.py
+++ src/python/zeroserver_datamesh/__init__.py
@@ -1,3 +1,12 @@
+"""
+
+look at Pyro 5 first, as that might allow for some of this to be mocked for initial use faster than a full P2p setup
+
+"""
+
+
+
+
 
 
 class Libp2p:
@@ -8,6 +17,8 @@ class Libp2p:
     pass
 
 
+
+
 @dataclasses
 class DataMeshPeer:
     """
@@ -16,9 +27,6 @@ class DataMeshPeer:
     authentication
 
     """
-    
-
-
 
     @classmethod
     def get_instance(cls): pass
@@ -26,52 +34,3 @@ class DataMeshPeer:
 
 
 
-class PubSubEventType(Enum):
-    
-
-class NotificationHandler:
-
-    await def handle_pubsub(self,
-        pubsub_key: str,
-        pubsub_event_type: PubSubEventType,
-        pubsub_data: dict) -> None: pass
-
-
-class StateHandle:
-    """
-
-    messaging encryption, underlying backend (Supabase, Ethereum, etc.) and setup (login, auth, password, etc.) should be abstrcated behind this class, such that once an app has an initialized version of this object, it is assumed that everything needed to set that up has already magically happened in the backgroun
-
-    in this system, there is only pubsub.  a messaging setup is just a two party pubsub
-
-    """
-
-
-    await def block_read(self,position,length) -> byte[]: pass
-    await def block_write(self,position,length,data) -> None: pass
-
-    await def pubsub_send(self,pubsub_key,data,callback_fn) -> None: pass
-    await def pubsub_subscribe(self,
-        pubsub_key: str,
-        notification_handler: NotificationHandler) -> None: pass
-
-    await def pubsub_unsubscribe(self,
-        pubsub_key: str,
-        notification_handler: NotificationHandler) -> None: pass
-
-    await def get_local_kvstore(self) -> dict: 
-        """used to get the local version of the key-value store used for things like data that needs to go into the UI
-        because the dict does not have a notification mechansim, a Pubsub should be used instead of polling to notify when to process new information
-        """
-        pass
-
-
-
-
-class SqlaProxy:
-    """
-
-    since SqlAlchemy is available in Pyodide, it makes sense to think about having the underlying backend proxied over ZeroServer Data Mesh
-
-    """
-    pass
